95f25cfae90eec46a2a9355f2d66f93f219ef23a
enriclem <e.clementi@aleide.it>
2026-03-25 17:54:07 +0100

    Fix grafiche

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     @include('partials.teaser', ['title' => 'Login'])
 
-    <section>
+    <section class="mb-5 pb-5">
         <div class="container">
             <div class="first-section">
                 @if ($contestEnded)
@@ -21,17 +21,18 @@
                         <a href="{{ route('home') }}" class="btn-mokador">Torna alla home</a>
                     </div>
                 @else
-                <h2 class="section-title-dark">Accedi per giocare</h2>
-                <p class="text-subtitle text-center">Se hai già un account, accedi per tentare la fortuna!<br>
-                    Se non hai un account puoi <a href="{{ route('register') }}">registrarti qui</a>.</p>
-                <form action="{{ route('login') }}" method="POST" class="form-punti-vendita">
-                    @csrf
-                    <input type="email" name="email" placeholder="Email" value="{{ old('email') }}" required>
-                    <input type="password" name="password" placeholder="Password" required>
-                    <button type="submit" class="btn-mokador">Accedi</button>
-                </form>
-                <p class="text-subtitle text-center"><a href="{{ route('password.request') }}">Password dimenticata?</a></p>
-                <p class="text-subtitle text-center pb-4">Non hai un account? <a href="{{ route('register') }}" class="btn-mokador ms-0 mt-2 mt-md-0 ms-md-3">Registrati</a></p>
+                    <h2 class="section-title-dark">Accedi per giocare</h2>
+                    <p class="text-subtitle text-center">Se hai già un account, accedi per tentare la fortuna!<br>
+                        Se non hai un account puoi <a href="{{ route('register') }}">registrarti qui</a>.</p>
+                    <form action="{{ route('login') }}" method="POST" class="form-punti-vendita">
+                        @csrf
+                        <input type="email" name="email" placeholder="Email" value="{{ old('email') }}" required>
+                        <input type="password" name="password" placeholder="Password" required>
+                        <button type="submit" class="btn-mokador">Accedi</button>
+                    </form>
+                    <p class="text-subtitle text-center"><a href="{{ route('password.request') }}">Password dimenticata?</a></p>
+                    <p class="text-subtitle text-center pb-4">Non hai un account? <a href="{{ route('register') }}"
+                            class="btn-mokador ms-0 mt-2 mt-md-0 ms-md-3">Registrati</a></p>
                 @endif
             </div>
         </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,8 @@
             <div class="teaser-home-overlay"></div>
             <div class="teaser-home-content">
                 <img class="d-none d-md-flex teaser-home-persone" src="{{ asset('img/teaser-persone.png') }}" alt="Persone">
-                <img class="teaser-home-logo" src="{{ asset('img/mokador-concorso-logo.svg') }}" alt="Mokador Concorso Logo">
+                <img class="teaser-home-logo" src="{{ asset('img/mokador-concorso-logo.svg') }}"
+                    alt="Mokador Concorso Logo">
             </div>
         </div>
     </section>
@@ -56,7 +57,8 @@
             </div>
             <div class="d-flex justify-content-center align-items-center flex-column py-5 gap-4">
                 <h2 class="section-title-light pt-5">Premi settimanali</h2>
-                <p class="text-light text-center text-subtitle">Ogni giorno <strong>tenta la fortuna</strong>:<br>scopri subito se hai vinto!</p>
+                <p class="text-light text-center text-subtitle">Ogni giorno <strong>tenta la fortuna</strong>:<br>scopri
+                    subito se hai vinto!</p>
                 <div class="grid-five-card">
                     <div class="product-card">
                         <div class="card-content">
@@ -117,9 +119,14 @@
                 <div class="d-flex flex-column gap-3 align-items-center justify-content-center order-1 order-lg-2">
                     <p class="prize-numeration">1° PREMIO</p>
                     <h3 class="section-subtitle">soggiorno per 2 persone<br>al Family Hotel Gran Baita</h3>
-                    <p class="text-subtitle text-center">Il primo premio è <strong>una settimana per 2 persone</strong> presso il <a href="https://www.familyhotelgranbaita.it/" target="_blank" rel="noopener">Family Hotel Gran Baita</a>, immerso nella splendida Val di Fassa, esplora e rilassati tra i paesaggi suggestivi delle Dolomiti.</p>
-                    <p class="text-subtitle text-center">Il soggiorno è valido esclusivamente dall'11 al 18 luglio 2026. Il vincitore dovrà confermare la propria presenza direttamente all'hotel entro il 30 giugno 2026.</p>
-                    <p class="text-subtitle text-center"><strong>Non sono incluse le spese di viaggio e i servizi non espressamente ricompresi</strong></p>
+                    <p class="text-subtitle text-center">Il primo premio è <strong>una settimana per 2 persone</strong>
+                        presso il <a href="https://www.familyhotelgranbaita.it/" target="_blank" rel="noopener">Family Hotel
+                            Gran Baita</a>, immerso nella splendida Val di Fassa, esplora e rilassati tra i paesaggi
+                        suggestivi delle Dolomiti.</p>
+                    <p class="text-subtitle text-center">Il soggiorno è valido esclusivamente dall'11 al 18 luglio 2026. Il
+                        vincitore dovrà confermare la propria presenza direttamente all'hotel entro il 30 giugno 2026.</p>
+                    <p class="text-subtitle text-center"><strong>Non sono incluse le spese di viaggio e i servizi non
+                            espressamente ricompresi</strong></p>
                 </div>
             </div>
         </div>
@@ -132,8 +139,10 @@
                 <div class="d-flex flex-column gap-3 align-items-center justify-content-center">
                     <p class="prize-numeration">2° PREMIO</p>
                     <h3 class="section-subtitle">Macchina caffè<br>Mokador Diva E1</h3>
-                    <p class="text-subtitle text-center">Il secondo premio è la <strong>macchina caffè Diva E1 di Mokador, compatta ed elegante</strong>, perfetta per un espresso cremoso come al bar.</p>
-                    <p class="text-subtitle text-center">In regalo anche <strong>100 capsule di caffè</strong> per iniziare subito a gustarla.</p>
+                    <p class="text-subtitle text-center">Il secondo premio è la <strong>macchina caffè Diva E1 di Mokador,
+                            compatta ed elegante</strong>, perfetta per un espresso cremoso come al bar.</p>
+                    <p class="text-subtitle text-center">In regalo anche <strong>100 capsule di caffè</strong> per iniziare
+                        subito a gustarla.</p>
                 </div>
                 <img class="prize-img-cream" src="{{ asset('img/macchina-caffe.png') }}" alt="">
             </div>
@@ -148,7 +157,8 @@
                 <div class="d-flex flex-column gap-3 align-items-center justify-content-center order-1 order-lg-2">
                     <p class="prize-numeration">3° PREMIO</p>
                     <h3 class="section-subtitle">Montalatte</h3>
-                    <p class="text-subtitle text-center">Il terzo premio è un pratico <strong>montalatte</strong>, perfetto per preparare a casa cappuccini cremosi e soffici schiume di latte come al bar.</p>
+                    <p class="text-subtitle text-center">Il terzo premio è un pratico <strong>montalatte</strong>, perfetto
+                        per preparare a casa cappuccini cremosi e soffici schiume di latte come al bar.</p>
                 </div>
             </div>
         </div>
@@ -159,8 +169,11 @@
         <div class="container py-5">
             <div class="d-flex flex-column gap-4 align-items-center justify-content-center py-5">
                 <h2 class="section-title-dark text-center">Gioca ora</h2>
-                <p class="text-subtitle text-center">Gioca subito, hai una <strong>opportunità giornaliera</strong> per vincere <strong>fantastici premi</strong><br>e alla fine parteciperai all'<strong>estrazione finale</strong>!</p>
-                <a href="{{ auth()->check() ? route('game.show') : route('register') }}" class="btn-mokador">Registrati e Vinci</a>
+                <p class="text-subtitle text-center">Gioca subito, hai una <strong>opportunità giornaliera</strong> per
+                    vincere <strong>fantastici premi</strong><br>e alla fine parteciperai all'<strong>estrazione
+                        finale</strong>!</p>
+                <a href="{{ auth()->check() ? route('game.show') : route('register') }}" class="btn-mokador">Registrati e
+                    Vinci</a>
             </div>
         </div>
     </section>

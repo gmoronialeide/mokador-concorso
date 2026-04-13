58869925c16f711da738c67e7670fd99c2fd0392
guidomoroni <g.moroni@aleide.it>
2026-04-13 16:33:46 +0200

    robot index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    @production
+    <meta name="robots" content="index, follow">
+    @else
+    <meta name="robots" content="noindex, nofollow">
+    @endproduction
     <title>@yield('title', 'Mokador ti porta in vacanza')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
